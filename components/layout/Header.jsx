1120b76a8efdff8ec4dc1010b8468ce6fa66c158
Ho Tien <hotien2107@gmail.com>
2022-01-10 15:31:35 +0700

    add edit profile

--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useState } from "react";
 import { FaBell, FaCommentAlt, FaHeart } from "react-icons/fa";
 import { VscEdit } from "react-icons/vsc";
 import useFirebaseAuth from "../../hooks/use-auth";
@@ -14,26 +15,26 @@ function Header() {
   const { authUser, logout } = auth;
 
   return (
-    <div className="w-full flex justify-center content-center border-b-[0.5px] bg-white">
-      <div className="h-24 w-4/5 flex justify-between max-w-screen-xl">
-        <div className="h-full w-20 flex items-center">
-          <div className="h-4/5 w-full">
-            <Image src="/svg/logo.svg" layout="responsive" height="64" width="64" alt="" />
+    <div className='w-full flex justify-center content-center border-b-[0.5px] bg-white'>
+      <div className='h-24 w-4/5 flex justify-between max-w-screen-xl relative'>
+        <div className='h-full w-20 flex items-center'>
+          <div className='h-4/5 w-full'>
+            <Image src='/svg/logo.svg' layout='responsive' height='64' width='64' alt='' />
           </div>
         </div>
 
-        <div className="flex items-center w-1/2 justify-around">
-          <Link href="/">
-            <div className="font-bold text-purple-900 underline underline-offset-4 cursor-pointer">Trang chủ</div>
+        <div className='flex items-center w-1/2 justify-around'>
+          <Link href='/'>
+            <div className='font-bold text-purple-900 underline underline-offset-4 cursor-pointer'>Trang chủ</div>
           </Link>
           <div>Mẹo tuyển dụng</div>
           <div>Template CV</div>
         </div>
 
         {!authUser ? (
-          <div className="flex items-center">
+          <div className='flex items-center'>
             <button
-              className="p-3 rounded-lg bg-purple-900 text-white font-bold transition-all duration-200 hover:bg-purple-800"
+              className='p-3 rounded-lg bg-purple-900 text-white font-bold transition-all duration-200 hover:bg-purple-800'
               onClick={() => router.push("/auth/login")}
             >
               Login
@@ -44,7 +45,7 @@ function Header() {
             {/* chatting - start */}
             <div className='flex'>
               <div className='flex items-center mr-5'>
-                <Link href={'/chat/' + chatList[0]?.id}>
+                <Link href={"/chat/" + chatList[0]?.id}>
                   <div className='bg-gray-200 rounded-full p-3 text-purple-900 cursor-pointer transition-all duration-200 hover:bg-purple-900 hover:text-white'>
                     <FaCommentAlt className='text-xl' />
                   </div>
@@ -53,26 +54,27 @@ function Header() {
               {/* chatting - end */}
 
               {/* notification - start */}
-              <div className="flex items-center mr-5">
-                <div className="bg-gray-200 rounded-full p-3 text-purple-900 cursor-pointer transition-all duration-200 hover:bg-purple-900 hover:text-white">
-                  <FaBell className="text-2xl" />
+              <div className='flex items-center mr-5'>
+                <div className='bg-gray-200 rounded-full p-3 text-purple-900 cursor-pointer transition-all duration-200 hover:bg-purple-900 hover:text-white'>
+                  <FaBell className='text-2xl' />
                 </div>
               </div>
               {/* notification - end */}
               {authUser.role == 2 ? (
-                <div className="flex items-center" onClick={() => router.push("/hr/posts/create")}>
-                  <div className="bg-gray-200 rounded-full p-3 text-purple-900 cursor-pointer transition-all duration-200 hover:bg-purple-900 hover:text-white">
-                    <VscEdit className="text-xl" />
+                <div className='flex items-center' onClick={() => router.push("/hr/posts/create")}>
+                  <div className='bg-gray-200 rounded-full p-3 text-purple-900 cursor-pointer transition-all duration-200 hover:bg-purple-900 hover:text-white'>
+                    <VscEdit className='text-xl' />
                   </div>
                 </div>
               ) : (
-                <div className="flex items-center" onClick={() => router.push("/candidate/favorites")}>
-                  <div className="bg-gray-200 rounded-full p-3 text-purple-900 cursor-pointer transition-all duration-200 hover:bg-purple-900 hover:text-white">
-                    <FaHeart className="text-xl" />
+                <div className='flex items-center' onClick={() => router.push("/candidate/favorites")}>
+                  <div className='bg-gray-200 rounded-full p-3 text-purple-900 cursor-pointer transition-all duration-200 hover:bg-purple-900 hover:text-white'>
+                    <FaHeart className='text-xl' />
                   </div>
                 </div>
               )}
             </div>
+            
             {/* user - start */}
             <div className='flex items-center'>
               <div className='cursor-pointer' onClick={() => setIsShowUserMenu(!isShowUserMenu)}>
@@ -88,8 +90,13 @@ function Header() {
             {/* user - start */}
 
             {isShowUserMenu ? (
-              <ul className='absolute top-full right-0 text-center bg-white rounded-md shadow-lg cursor-pointer overflow-hidden' onClick={() => setIsShowUserMenu(false)}>
-                <li className='text-xl font-medium hover:text-white hover:bg-purple-600 p-2'>Thông tin cá nhân</li>
+              <ul
+                className='absolute top-full right-0 text-center bg-white rounded-md shadow-lg cursor-pointer overflow-hidden z-50'
+                onClick={() => setIsShowUserMenu(false)}
+              >
+                <Link href={"/profile"}>
+                  <li className='text-xl font-medium hover:text-white hover:bg-purple-600 p-2'>Thông tin cá nhân</li>
+                </Link>
                 <li className='text-xl font-medium hover:text-white hover:bg-purple-600 p-2' onClick={logout}>
                   Đăng xuất
                 </li>
